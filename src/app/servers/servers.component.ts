129fa489a78fbf4678b81da0f5b5a7f83b255722
Kim Hyeoncheol <effectmix@gmail.com>
2020-07-11 00:02:03 +0900

    The Basics - ngIf with an Else condition Directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -10,6 +10,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
+  serverCreated = false;
 
   constructor() {
     /* () => ... means function() {} */
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
